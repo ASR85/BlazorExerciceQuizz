f8d8d28af6d7f4f150c52c535796370c1697684e
Arnaud L.Celestino <arnaud.1985@hotmail.com>
2025-02-13 12:32:13 +0100

    Ajoutez des fichiers projet.

---
+++ Model/Personne.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorExerciceQuizz.Model
+{
+    public class Personne
+    {
+        [Required]
+        public string Nom { get; set; }
+
+
+        [Required]
+        public string Prenom { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateNaissance { get; set; }
+
+
+        public string TypePersonne { get; set; }
+
+
+        public string Couleur { get; set; }
+    }
+
+
+    public enum TypePersonne
+    {
+    }
+}
